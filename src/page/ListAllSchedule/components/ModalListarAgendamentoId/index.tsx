dcd65e4485130eff4bf3043a1bb4c47fb68dea43
Jarbson <jarbson.costa@Crea.Local>
2023-10-24 08:47:10 -0300

    refactor: corrigindo formatação de data e hora

--- src/page/ListAllSchedule/components/ModalListarAgendamentoId/index.tsx
+++ src/page/ListAllSchedule/components/ModalListarAgendamentoId/index.tsx
@@ -6,7 +6,7 @@ import { formatarData } from "../../../../utils/formatDate";
 
 export function ModalListarAgendamentoId(props) {
   const data = props.data;
-  console.log(data);
+
   // Obtendo a data e hora atual
   const dataAtual = new Date(); // Filtrando os objetos com hora_fim maior ou igual à data e hora atual
   const objetosFiltrados = data?.exibicoes.filter((objeto) => {
@@ -14,6 +14,7 @@ export function ModalListarAgendamentoId(props) {
     return horaFim >= dataAtual;
   });
 
+  console.log(objetosFiltrados);
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Body style={{ padding: "0" }}>
@@ -35,35 +36,16 @@ export function ModalListarAgendamentoId(props) {
             {data?.exibicoes.length > 0 ? (
               <>
                 {objetosFiltrados?.map((item, index) => {
-                  // Separar a data e a hora usando o método split()
-                  var partes = item.hora_inicio.split("T");
-                  var data = partes[0]; // Parte antes do 'T' é a data
-                  var hora = partes[1]; // Parte depois do 'T' é a hora
-
-                  var dataObjeto = new Date(data);
-
-                  // Obter dia, mês e ano do objeto de data
-                  var dia = dataObjeto.getDate(); // 20
-                  var mes = dataObjeto.getMonth() + 1; // Mês é base 0 (janeiro é 0), então adicionamos 1
-                  var ano = dataObjeto.getFullYear(); // 2023
-
-                  // Formatar dia e mês com dois dígitos (por exemplo, 01, 02, ..., 09)
-                  dia = dia < 10 ? "0" + dia : dia;
-                  mes = mes < 10 ? "0" + mes : mes;
-
-                  // Formatar a data no formato desejado (DD-MM-AAAA)
-                  var dataFormatada = dia + "/" + mes + "/" + ano;
-
-                  // Obtendo apenas a parte da hora da string hora_fim
                   const horaFimString = item.hora_fim
                     .split("T")[1]
                     .split(".")[0];
                   return (
                     <span key={index}>
-                      <strong >{index+1} - Dia : </strong>
-                      <span>{dataFormatada} </span> <strong>Hora : </strong>
-                      {hora} <strong> até</strong>
-                      {horaFimString}
+                      <strong>
+                        {index + 1} - Dia : {formatarData(item.hora_inicio)}
+                      </strong >
+                      &nbsp;até&nbsp;
+                      <strong>{horaFimString}</strong>
                     </span>
                   );
                 })}
